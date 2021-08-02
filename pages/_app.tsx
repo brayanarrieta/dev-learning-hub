9c04f75286cbd9b2e090b7fb369c5cdd497df050
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-08-02 14:31:07 -0600

    Integrate Eslint with Nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <UserProvider>
     <ChakraProvider>
-             <Component {...pageProps} />
+      <Component {...pageProps} />
     </ChakraProvider>
   </UserProvider>
 );
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-import { useUser } from "@auth0/nextjs-auth0";
+import { useUser } from '@auth0/nextjs-auth0';
 
 const Home = () => {
   const { user, error, isLoading } = useUser();
- 
+
   if (isLoading) return <div>Loading...</div>;
- 
+
   if (error) return <div>{error.message}</div>;
- 
+
   if (user) {
     return (
       <div>
@@ -18,6 +18,6 @@ const Home = () => {
     );
   }
   return <a href="/api/auth/login">Login</a>;
- };
+};
 
 export default Home;
