85870a3014b7b92bebbacd26c1e54625c5df9215
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-08-02 13:43:36 -0600

    Add initial Auth0 integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <UserProvider>
+      <Component {...pageProps} />
+
+  </UserProvider>
+);
+
 export default MyApp
