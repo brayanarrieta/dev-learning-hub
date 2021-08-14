0e4f74f89e244be3a15943898add0b823e6b962e
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-08-13 18:39:48 -0600

    Add react hooks form integration

--- pages/community-requests/create.tsx
+++ pages/community-requests/create.tsx
@@ -1,6 +1,7 @@
 import {
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
@@ -10,12 +11,22 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { CommunityRequestType } from '../../constants/enums';
 import SidebarWithHeader from '../../custom-components/Layout/SidebarWithHeader';
+
+interface IFormInput {
+  title: string;
+}
 
 const CommunityRequestsAdd = () => {
   // eslint-disable-next-line no-unused-vars
   const [type, setType] = useState<string | null>(null);
+  const { control, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
 
   return (
     <SidebarWithHeader>
@@ -31,28 +42,50 @@ const CommunityRequestsAdd = () => {
           p={4}
           borderRadius="lg"
         >
-          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
-            <FormControl>
-              <FormLabel>Title</FormLabel>
-              <Input />
-            </FormControl>
 
-            <FormControl>
-              <FormLabel>Type</FormLabel>
-              <Select placeholder="Select the type" onChange={(e) => setType(e.target.value)}>
-                <option value={CommunityRequestType.INTERVIEW_QUESTION}>
-                  {CommunityRequestType.INTERVIEW_QUESTION}
-                </option>
-                <option value={CommunityRequestType.CODE_SNIPPET}>
-                  {CommunityRequestType.CODE_SNIPPET}
-                </option>
-                <option value={CommunityRequestType.COURSE}>
-                  {CommunityRequestType.COURSE}
-                </option>
-              </Select>
-            </FormControl>
+          <form onSubmit={handleSubmit(onSubmit)}>
 
-          </SimpleGrid>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+
+              <Controller
+                name="title"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: 'Title field is required',
+                  minLength: {
+                    value: 10,
+                    message: 'Title must have a min length of 10 characters',
+                  },
+                }}
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                  <FormControl isInvalid={!!error}>
+                    <FormLabel>Title</FormLabel>
+                    <Input onChange={onChange} value={value} />
+                    {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+                  </FormControl>
+                )}
+              />
+
+              <FormControl>
+                <FormLabel>Type</FormLabel>
+                <Select placeholder="Select the type" onChange={(e) => setType(e.target.value)}>
+                  <option value={CommunityRequestType.INTERVIEW_QUESTION}>
+                    {CommunityRequestType.INTERVIEW_QUESTION}
+                  </option>
+                  <option value={CommunityRequestType.CODE_SNIPPET}>
+                    {CommunityRequestType.CODE_SNIPPET}
+                  </option>
+                  <option value={CommunityRequestType.COURSE}>
+                    {CommunityRequestType.COURSE}
+                  </option>
+                </Select>
+              </FormControl>
+
+            </SimpleGrid>
+
+            <input type="submit" />
+          </form>
 
         </Stack>
 
