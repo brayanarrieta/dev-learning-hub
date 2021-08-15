6bd01fad6abab845758f1ce2873bb210c5d939f7
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2021-08-14 18:31:34 -0600

    Remove dependency and fix issue

--- dal/interviewQuestionRepository.ts
+++ dal/interviewQuestionRepository.ts
@@ -8,7 +8,7 @@ export const getInterviewQuestionsByTechnologyIdWithPaginationDal = async (
   technologyId: string, currentPage: number, pageSize: number,
 ) => {
   const offset = (currentPage - 1) * pageSize;
-  const interviewQuestions = await InterviewQuestion.find({ technologyId }).skip(offset)
+  const interviewQuestions = await InterviewQuestion.find({ technology: technologyId }).skip(offset)
     .limit(pageSize);
   return interviewQuestions;
 };
@@ -16,6 +16,6 @@ export const getInterviewQuestionsByTechnologyIdWithPaginationDal = async (
 export const truncateInterviewQuestions = async () => InterviewQuestion.deleteMany();
 
 export const getInterviewQuestionsCountByTechnologyIdDal = async (technologyId: string) => {
-  const count = await InterviewQuestion.find({ technologyId }).countDocuments();
+  const count = await InterviewQuestion.find({ technology: technologyId }).countDocuments();
   return count;
 };
